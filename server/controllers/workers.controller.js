47e83fcb25b4c67c6abecd554fead83ee8a31e36
Bridger Avery <bridger.avery@outlook.com>
2024-06-17 21:25:12 -0600

    Refactored SQL calls to one call to reduce duplicate code

--- server/controllers/workers.controller.js
+++ server/controllers/workers.controller.js
@@ -6,71 +6,9 @@ const db = new dbHelper();
 
 export default class workerController {
     async getWorkerCost(req, res) {
-            // Parse body and create where filters :D
-            let workerIDs = req.body.worker_ids;
-            let locationIDs = req.body.location_ids;
-            let isComplete = req.body.isComplete;
-            let whereCondition = ``;
-            let workerWhereCondition = ``;
-            
-            // Build our WHERE filter for worker IDs
-            if (Array.isArray(workerIDs) && workerIDs.length > 0) {
-                if (workerIDs.length == 1) {
-                    workerWhereCondition = `worker.id = ${workerIDs[0]}`
-                } else {
-                    let joinedWorkerIDs = workerIDs.join(", ")
-                    workerWhereCondition = `worker.id IN (${joinedWorkerIDs})`
-                }
-            }
-            let locationWhereCondition = ``;
-            // Build our WHERE filter for location IDs
-            if (Array.isArray(locationIDs) && locationIDs.length > 0) {
-                if (locationIDs.length == 1) {
-                    locationWhereCondition = `location.id = ${locationIDs[0]}`
-                } else {
-                    let joinedLocationIDs = locationIDs.join(", ")
-                    locationWhereCondition = `location.id IN (${joinedLocationIDs})`
-                }
-            }
-            let isCompleteCondition = ``;
-            // Build our WHERE filter for isComplete
-            if (isComplete == "complete") {
-                isCompleteCondition = `task.is_complete = TRUE`
-            } else if (isComplete == "incomplete") {
-                isCompleteCondition = `task.is_complete = FALSE`
-            }
+            let whereCondition = await db.buildWhereClause(req)
 
-            if (workerWhereCondition !== `` || locationWhereCondition !== `` || isCompleteCondition !== ``) {
-                // One of our where conditions is not empty
-                let isFirstCondition = true
-                whereCondition += "WHERE "
-                if (workerWhereCondition !== ``) {
-                        isFirstCondition = false
-                        whereCondition += workerWhereCondition;
-                }
-                if (locationWhereCondition !== ``) {
-                    if (!isFirstCondition) {
-                        isFirstCondition = false
-                        whereCondition += " AND "
-                    }
-                    whereCondition += locationWhereCondition
-                }
-                if (isCompleteCondition !== ``) {
-                    if (!isFirstCondition) {
-                        isFirstCondition = false
-                        whereCondition += " AND "
-                    }
-                    whereCondition += isCompleteCondition
-                }
-            }
-
-            let resp = ''
-
-            if (isCompleteCondition !== `` || locationWhereCondition !== ``) {
-                resp = await db.getFilteredWorkerCost(whereCondition)
-            } else {
-                resp = await db.getUnfilteredWorkerCost(whereCondition)
-            }
+            let resp = await db.getWorkerCost(whereCondition)
 
             // Let's just return this now to see if it works haha
             console.log(resp)
--- server/helper/db.js
+++ server/helper/db.js
@@ -1,48 +1,69 @@
 import * as mariadb from "mariadb";
 
 export default class dbHelper {
-    async getUnfilteredWorkerCost(whereQuery) {
-        let dbConn
-        try {
-            dbConn = mariadb.createPool({
-                host: process.env["DATABASE_HOST"],
-                user: process.env["DATABASE_USER"],
-                password: process.env["DATABASE_PASSWORD"],
-                database: process.env["DATABASE_NAME"]
-            })
-
-            // BUILD an in array like before than use that
-
-            let query = `
-                SELECT
-                    worker.username AS name,
-                    worker.id AS worker_ID,
-                    ROUND((worker.hourly_wage * (SUM(logged_time.time_seconds) / 3600)), 2) AS worker_total
-                FROM
-                    limble.workers AS worker
-                INNER JOIN
-                    limble.logged_time AS logged_time
-                ON
-                    worker.id = logged_time.worker_id
-                ${whereQuery}
-                GROUP BY
-                    worker.id;
-                `
-
-            let dbResp = await dbConn.query(query)
-            if (dbResp.length < 1) {
-                return "no results found with the given filter"
+    async buildWhereClause(req) {
+        // Parse body and create where filters :D
+        let workerIDs = req.body.worker_ids;
+        let locationIDs = req.body.location_ids;
+        let isComplete = req.body.isComplete;
+        let whereCondition = ``;
+        let workerWhereCondition = ``;
+        
+        // Build our WHERE filter for worker IDs
+        if (Array.isArray(workerIDs) && workerIDs.length > 0) {
+            if (workerIDs.length == 1) {
+                workerWhereCondition = `worker.id = ${workerIDs[0]}`
+            } else {
+                let joinedWorkerIDs = workerIDs.join(", ")
+                workerWhereCondition = `worker.id IN (${joinedWorkerIDs})`
             }
-            return dbResp
-        } catch(error) {
-            console.log("We had an error:", error)
-            res.json(error)
-        } finally {
-            if (dbConn) dbConn.end();
         }
+        let locationWhereCondition = ``;
+        // Build our WHERE filter for location IDs
+        if (Array.isArray(locationIDs) && locationIDs.length > 0) {
+            if (locationIDs.length == 1) {
+                locationWhereCondition = `location.id = ${locationIDs[0]}`
+            } else {
+                let joinedLocationIDs = locationIDs.join(", ")
+                locationWhereCondition = `location.id IN (${joinedLocationIDs})`
+            }
+        }
+        let isCompleteCondition = ``;
+        // Build our WHERE filter for isComplete
+        if (isComplete == "complete") {
+            isCompleteCondition = `task.is_complete = TRUE`
+        } else if (isComplete == "incomplete") {
+            isCompleteCondition = `task.is_complete = FALSE`
+        }
+
+        if (workerWhereCondition !== `` || locationWhereCondition !== `` || isCompleteCondition !== ``) {
+            // One of our where conditions is not empty
+            let isFirstCondition = true
+            whereCondition += "WHERE "
+            if (workerWhereCondition !== ``) {
+                    isFirstCondition = false
+                    whereCondition += workerWhereCondition;
+            }
+            if (locationWhereCondition !== ``) {
+                if (!isFirstCondition) {
+                    isFirstCondition = false
+                    whereCondition += " AND "
+                }
+                whereCondition += locationWhereCondition
+            }
+            if (isCompleteCondition !== ``) {
+                if (!isFirstCondition) {
+                    isFirstCondition = false
+                    whereCondition += " AND "
+                }
+                whereCondition += isCompleteCondition
+            }
+        }
+
+        return whereCondition
     }
 
-    async getFilteredWorkerCost(whereClause) {
+    async getWorkerCost(whereClause) {
         let dbConn
         try {
             dbConn = mariadb.createPool({
